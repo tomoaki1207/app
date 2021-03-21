4b0ddc638c9cf2a87634aba3750d798090a0ab44
tomoaki1207 <79908659+tomoaki1207@users.noreply.github.com>
2021-03-21 07:35:25 +0000

    create top

---
+++ Laravel/database/migrations/2021_03_17_234302_add_column_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('self_introduction',500)->nullable();
+            $table->tinyInteger('sex')->default(0);
+            $table->integer('area')->
+            $table->integer('age')
+            $table->integer('want_talk')
+            $table->integer('can_talk')
+            $table->string('職業')
+            $table->string('img_name')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
